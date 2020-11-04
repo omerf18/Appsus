ca2815d7a228c8c974b7c49975f2d416522e5206
omer <omerfussfeld@gmail.com>
2020-11-04 14:55:09 +0200

    start

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,6 @@
 import home from './pages/home.cmp.js';
 // import email from './pages/email.cmp.js';
-// import note from './pages/notes.cmp.js';
+import note from './pages/notes.cmp.js';
 
 const myRoutes = [
     {
@@ -11,10 +11,10 @@ const myRoutes = [
     //     path: '/email',
     //     component: email
     // },
-    // {
-    //     path: '/notes',
-    //     component: note
-    // }
+    {
+        path: '/notes',
+        component: note
+    }
 ];
 
 export const myRouter = new VueRouter({ routes: myRoutes });
