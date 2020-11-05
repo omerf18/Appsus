0570b6f28e2b283ce3e4f833a1595e7b52793426
omer <omerfussfeld@gmail.com>
2020-11-05 21:05:41 +0200

    as

--- js/notes/cmps/note-types/note-txt.cmp.js
+++ js/notes/cmps/note-types/note-txt.cmp.js
@@ -4,8 +4,8 @@ export default {
     template: `
     <section>
        <div class="note flex column">
-            <input class="note-title btn" type="text" placeholder="Dont forget!" @change="edit()" v-model="note.title" maxlength="18">
-            <textarea class="note-txt btn" rows="12" cols="6" @change="edit(note.id)" v-model="note.info"></textarea>
+            <input class="note-title btn" type="text" placeholder="Dont forget!" @change="edit" v-model="note.title" maxlength="18">
+            <textarea class="note-txt btn" rows="12" cols="6" @change="edit" v-model="note.info"></textarea>
             <div class="flex space-between">
                 <div class="note-bgc btn"> </div>
                 <div class="note-pin btn"> </div>
