1a568e28bbbe1938e1e9ba96e40866bd299ad596
Dennis Nemirovski <dennisnemirovski@gmail.com>
2020-11-05 22:22:40 +0200

    css

--- js/email/cmps/email-peek.cmp.js
+++ js/email/cmps/email-peek.cmp.js
@@ -5,15 +5,17 @@ export default {
     props: ['email'],
     template: `
     
-        <li class="email-peek flex row space-between">
+        <li class="email-peek flex column space-between">
                 <div class="peek-header flex space-between">
                     <h3 class="peek-subject"> {{ email.subject }} </h3>
                     <div class=peek-controls>
                         <button @click.prevent ="removeEmail">delete</button>  <router-link :to="'email/' + email.id">expand</router-link>
                     </div>
                 </div>
-                <p class="peek-from"> {{ email.name }} <span>{{email.fromEmail}}</span></p>
-                <p class="peek-body"> {{ email.body }} </p>
+                <div class="peek-content">
+                    <p class="peek-from"> {{ email.name }} <small class= "from-email" >{{email.fromEmail}}</small></p>
+                    <p class="peek-body"> {{ email.body }} </p>
+                </div>
             </li>
     `,
     methods: {
