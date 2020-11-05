ef7d05e5903dc86e55729c9d32858ad3621a072b
omer <omerfussfeld@gmail.com>
2020-11-05 12:18:38 +0200

    a

--- js/notes/cmps/note-video.cmp.js
+++ js/notes/cmps/note-video.cmp.js
@@ -3,9 +3,20 @@ export default {
     template: `
     <section>
        <div class="note flex column">
-            <input class="note-title" type="text" maxlength="18" v-model="title">
+            <input class="note-title" type="text" maxlength="20" v-model="title">
+<!-- 
+            <iframe id="ytplayer" type="text/html" width="640" height="360"
+            src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+            frameborder="0"></iframe>
+            <div id="ytplayer"></div> -->
 
+            <iframe width="420" height="315"
+            src="https://www.youtube.com/watch?v=5LYrN_cAJoA&list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa">
+            </iframe>
          
+
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/5LYrN_cAJoA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
         </div>
     </section>
  `,
@@ -20,5 +31,5 @@ export default {
             }
         }
     },
-}
 
+}
--- js/notes/pages/notes-app.cmp.js
+++ js/notes/pages/notes-app.cmp.js
@@ -2,12 +2,12 @@ import noteType from '../cmps/note-type.cmp.js';
 import noteTxt from '../cmps/note-txt.cmp.js';
 import noteImg from '../cmps/note-img.cmp.js';
 import noteTodos from '../cmps/note-todos.cmp.js';
-import noteVideo from '../cmps/note-todos.cmp.js';
+import noteVideo from '../cmps/note-video.cmp.js';
 
 export default {
     template: `
     <section>
-     <note-type @noteTypeSelect="getNoteInput($event)"></note-type>
+    <note-type @noteTypeSelect="getNoteInput($event)"></note-type>
     <note-txt/>
     <note-img/>
     <note-todos/>
