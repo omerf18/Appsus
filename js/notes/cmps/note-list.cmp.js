b734035d15901383a5d53dbc2d27630455462916
omer <omerfussfeld@gmail.com>
2020-11-07 00:02:07 +0200

    aA

--- js/notes/cmps/note-list.cmp.js
+++ js/notes/cmps/note-list.cmp.js
@@ -7,22 +7,41 @@ export default {
     name: 'noteList',
     props: ['notes'],
     template: `
-            <ul class="notes-list flex row">
-                   <li v-for="note in notes" :key="note.id">
-                        <component :note="note" :is="note.type" @colorNote="changeBgc" @editNote="noteToEdit" @removeNote="noteToRemove" @updateTodo="todoToUpdate">
-                             </component>
-                   </li>
-            </ul>
+        <section class="flex wrap space-between">
+        <div v-for="note in notes" :key="note.id">
+            <div class="note flex column space-between" :style="{ 'background-color': note.color }">
+                <input class="note-title btn" type="text" placeholder="Dont forget!" @change="edit" v-model="note.title" maxlength="18">
+                <component  :note="note" :is="note.type" @editNote="noteToEdit" @updateTodo="todoToUpdate"></component>
+                <div class="flex space-between">
+                    <div class="colorpick-container flex">
+                    <i class="fas fa-brush icon"></i>
+                        <div class="flex colorpick-container">
+                            <div class="note-bgc bgc0 btn" @click="changeBgc(note.id,'#fafafa')"></div>    
+                            <div class="note-bgc bgc1 btn" @click="changeBgc(note.id,'#9df3c4')"></div>
+                            <div class="note-bgc bgc2 btn" @click="changeBgc(note.id,'#b2dffb')"></div>
+                            <div class="note-bgc bgc3 btn" @click="changeBgc(note.id,'#e7a4e4')"></div>
+                            <div class="note-bgc bgc4 btn" @click="changeBgc(note.id,'#ffc55c')"></div>
+                        </div>
+                </div>
+                <i class="fas fa-trash-alt icon" @click="noteToRemove(note.id)"></i>
+                <i class="note-pin fas fa-thumbtack icon"></i>
+            </div>
+        </div>
+        </div>
+        </section>
     `,
-    methods: {
+        methods: {
+        edit() {
+            this.$emit('editNote');
+        },
         changeBgc(noteId, color) {
-            this.$emit('colorNote',noteId, color);
+            this.$emit('colorNote', noteId, color);
         },
         todoToUpdate(noteId, todoId) {
             this.$emit('updateTodo', noteId, todoId);
         },
         noteToRemove(noteId) {
-            this.$emit('removeNote',noteId);
+            this.$emit('removeNote', noteId);
         },
         noteToEdit() {
             this.$emit('editNote');
--- js/notes/cmps/note-types/note-img.cmp.js
+++ js/notes/cmps/note-types/note-img.cmp.js
@@ -3,38 +3,7 @@ export default {
     name: 'note-img',
     template: `
     <section>
-       <div class="note flex column" :style="{ 'background-color': note.color }">
-            <input class="note-title" type="text" placeholder="I love this image!" @change="edit" v-model="note.title" maxlength="22">
             <img class="note-img" :src="note.info">
-            <div class="flex space-between">
-                <div class="colorpick-container flex">
-                    <i class="fas fa-brush icon"></i>
-                    <div class="flex colorpick-container">
-                        <div class="note-bgc bgc0 btn" @click="color(note.id,'#fafafa')"></div>    
-                        <div class="note-bgc bgc1 btn" @click="color(note.id,'#9df3c4')"></div>
-                        <div class="note-bgc bgc2 btn" @click="color(note.id,'#b2dffb')"></div>
-                        <div class="note-bgc bgc3 btn" @click="color(note.id,'#e7a4e4')"></div>
-                        <div class="note-bgc bgc4 btn" @click="color(note.id,'#ffc55c')"></div>
-                    </div>
-            </div>
-            <i class="fas fa-trash-alt icon" @click="remove(note.id)"></i>
-            <i class="note-pin fas fa-thumbtack icon"></i>
-            </div>
-        </div>
     </section>
  `,
-    methods: {
-        color(noteId,color) {
-            this.$emit('colorNote',noteId, color);
-        },
-        color(noteId,color) {
-            this.$emit('colorNote',noteId, color);
-        },
-        remove(noteId) {
-            this.$emit('removeNote',noteId);
-        },
-        edit() {
-            this.$emit('editNote');
-        }
-    }
 }
