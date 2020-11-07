db6a5caf13e7bea4a508b42ee8058688e1d0a013
Dennis Nemirovski <dennisnemirovski@gmail.com>
2020-11-07 20:25:04 +0200

    home page done

--- js/cmps/sus-app-header.cmp.js
+++ js/cmps/sus-app-header.cmp.js
@@ -1,13 +1,13 @@
 export default {
     template: `
     <section class="sus-app-header">
-        <div class="container flex space-between">
-    <h1><img class="logo-horse" src="../img/logo-horse.png" alt=""> SusApp</h1>
-            <nav> 
-                <router-link to="/" exact >Home</router-link>
-                <router-link to="/email" exact >Email</router-link>
-                <router-link to="/notes" exact >Notes</router-link>
-                <router-link to="/about" exact >about</router-link>
+        <div class="header-content container flex space-between">
+        <router-link class="logo-link" to="/" exact ><h1 class="flex align-center"><img class="logo-horse" src="../img/logo-horse.png" alt=""> SusApp</h1></router-link>
+            <nav class="pages-link flex align-center"> 
+                <router-link class="align-self" to="/" exact >Home</router-link>
+                <router-link  to="/email" exact >Email</router-link>
+                <router-link   to="/notes" exact >Notes</router-link>
+                <router-link  to="/about" exact >about</router-link>
             </nav>
 </div>
     </section> 
