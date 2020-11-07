d968a70005426a0d7267af252ba05b4ee49e811f
Dennis Nemirovski <dennisnemirovski@gmail.com>
2020-11-07 21:59:45 +0200

    header

--- js/cmps/sus-app-header.cmp.js
+++ js/cmps/sus-app-header.cmp.js
@@ -2,7 +2,7 @@ export default {
     template: `
     <section class="sus-app-header">
         <div class="header-content container flex space-between">
-        <router-link class="logo-link" to="/" exact ><h1 class="flex align-center"><img class="logo-horse" src="../img/logo-horse.png" alt=""> SusApp</h1></router-link>
+        <router-link class="logo-link" to="/" exact ><h1 class="flex align-center"><img class="logo-horse" src="/img/logo-horse.png" alt=""> SusApp</h1></router-link>
             <nav class="pages-link flex align-center"> 
                 <router-link class="align-self" to="/" exact >Home</router-link>
                 <router-link  to="/email" exact >Email</router-link>
