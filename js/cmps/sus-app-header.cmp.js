062f7fd0711aa4c0c42ca155d51aac790404215b
Dennis Nemirovski <dennisnemirovski@gmail.com>
2020-11-07 20:08:13 +0200

    background

--- js/cmps/sus-app-header.cmp.js
+++ js/cmps/sus-app-header.cmp.js
@@ -2,7 +2,7 @@ export default {
     template: `
     <section class="sus-app-header">
         <div class="container flex space-between">
-    <h1>SusApp</h1>
+    <h1><img class="logo-horse" src="../img/logo-horse.png" alt=""> SusApp</h1>
             <nav> 
                 <router-link to="/" exact >Home</router-link>
                 <router-link to="/email" exact >Email</router-link>
--- js/pages/home.js
+++ js/pages/home.js
@@ -1,3 +1,33 @@
 export default {
-    
+    template: `
+  <section class= "flex justify-center column align-center">
+          <h1 class ="title">Let's Make It <span>Easy</span> Together</h1>
+          <div class="enter-apps flex row">
+              <router-link to="/email" class="enter-email flex column align-center"><span>EMAIL</span> <i class=" fas fa-envelope"></i></router-link>
+              <router-link to="/notes" class="enter-notes flex column align-center"><span>NOTES</span><i class="far fa-sticky-note"></i></router-link>
+          </div>
+  </section>
+  `,
+  
+    data() {
+        return {
+            
+            
+
+        }
+    },
+    computed: {
+      
+
+    },
+    methods: {
+
+    },
+       
+    created(){
+       
+    },
+    components: {
+      
+    }
 }
