8971e2cd65bd767f3d411b23e9e0eb0bb5a59a8a
omer <omerfussfeld@gmail.com>
2020-11-07 20:15:05 +0200

    aaa

--- js/pages/about.js
+++ js/pages/about.js
@@ -1,30 +1,26 @@
 
-
 export default {
-    template: `
+  template: `
   <section class = "email-app">
-
-  </section>`,
-  
-    data() {
-        return {
-            
-            
-
-        }
-    },
-    computed: {
-      
-
-    },
-    methods: {
-
-    },
-       
-    created(){
-       
-    },
-    components: {
-      
-    }
+    <div class="flex column space-between about-container">
+        <div class="about-page flex space-around">
+            <i class="far fa-envelope"></i>
+            <i class="far fa-sticky-note"></i>
+        </div>
+        <div class="about">
+              Welcome to SusApp! <br>
+              The perfect place for you to combine your notes & emails. <br>
+              So what are you waiting for? Take some notes and share with your friends! <br>
+              Enjoy our fantastic email services and take your notes with you everywhere you go, <br>
+              our responsive layout is perfect for your home computer and your personal smartphone! <br>
+              So what are you waiting for? Get started now!
+        </div>
+        <div class="flex about-page space-between">
+            <i class="fas fa-mobile-alt"></i>
+            <i class="fas fa-tablet-alt"></i>
+            <i class="fas fa-laptop"></i>
+        </div>
+  </div>
+  </section>
+  `
 }
