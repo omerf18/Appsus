0079e9fa0c08eff655dc498ed58311336410c889
omer <omerfussfeld@gmail.com>
2020-11-08 00:34:28 +0200

    aaa

--- js/cmps/sus-app-header.cmp.js
+++ js/cmps/sus-app-header.cmp.js
@@ -3,13 +3,27 @@ export default {
     <section class="sus-app-header">
         <div class="header-content container flex space-between">
         <router-link class="logo-link" to="/" exact ><h1 class="flex align-center"><img class="logo-horse" src="img/logo-horse.png" alt=""> SusApp</h1></router-link>
-            <nav class="pages-link flex align-center"> 
+            <nav class="nav pages-link flex align-center"> 
                 <router-link class="align-self" to="/" exact >Home</router-link>
                 <router-link  to="/email" exact >Email</router-link>
                 <router-link   to="/notes" exact >Notes</router-link>
                 <router-link  to="/about" exact >about</router-link>
             </nav>
+
+            <div class="phone-nav-btn btn" @click="showPhoneNav = !showPhoneNav">|||</div>
+            <nav class="phone-nav pages-link flex column align-center" :class="{'phone-nav-show': showPhoneNav}"> 
+                <router-link class="phone-nav-item align-self" to="/" exact >Home</router-link>
+                <router-link  class="phone-nav-item" to="/email" exact >Email</router-link>
+                <router-link  class="phone-nav-item" to="/notes" exact >Notes</router-link>
+                <router-link  class="phone-nav-item" to="/about" exact >about</router-link>
+            </nav>
 </div>
     </section> 
     `,
+    data() {
+        return {
+            showPhoneNav: false
+        }
+    }
 }
+
--- js/pages/about.js
+++ js/pages/about.js
@@ -4,21 +4,21 @@ export default {
   <section class = "email-app">
     <div class="flex column space-between about-container">
         <div class="about-page flex space-around">
-            <i class="far fa-envelope"></i>
-            <i class="far fa-sticky-note"></i>
+            <i class="about-icon far fa-envelope"></i>
+            <i class="about-icon far fa-sticky-note"></i>
         </div>
         <div class="about">
               Welcome to SusApp! <br>
               The perfect place for you to combine your notes & emails. <br>
-              So what are you waiting for? Take some notes and share with your friends! <br>
+              Take some notes and share with your friends! <br>
               Enjoy our fantastic email services and take your notes with you everywhere you go, <br>
               our responsive layout is perfect for your home computer and your personal smartphone! <br>
               So what are you waiting for? Get started now!
         </div>
         <div class="flex about-page space-between">
-            <i class="fas fa-mobile-alt"></i>
-            <i class="fas fa-tablet-alt"></i>
-            <i class="fas fa-laptop"></i>
+            <i class="about-icon fas fa-mobile-alt"></i>
+            <i class="about-icon fas fa-tablet-alt"></i>
+            <i class="about-icon fas fa-laptop"></i>
         </div>
   </div>
   </section>
